ad7d52759b055f62a25cffa898eab8b64a575ec9
Chr1sDev <chris@chr1s.dev>
2020-10-13 22:51:46 -0500

    stuff

--- commands/help.ts
+++ commands/help.ts
@@ -8,7 +8,7 @@ module.exports = {
         .setColor('#00FF86')
         .setTitle('Command List:')
         .setFooter('')
-        .setDescription('This is not done\nPing christopher#8888 to remind him\nthat he is procrastinating')
+        .setDescription('Check pins in #chat for list of commands')
         //.addFields(
         //    { name: '**1.** r!pfp - to get link for profile picture\n**2.** r!icon - gets server icon\n**3.** r!server-info\n**4.** r!user-info', value: 'page 1/1'}
         //)
---
+++ commands/test.ts
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "test",
+    execute(msg) {
+        msg.channel.send(`Test emote: <:animewave:765776072285356033>`);
+    }
+}
