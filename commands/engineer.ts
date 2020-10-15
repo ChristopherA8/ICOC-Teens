7939f984bc632afb7b2ff895b35b37ad6e06e29c
Chr1sDev <chris@chr1s.dev>
2020-10-15 10:52:40 -0500

    stuff

--- commands/engineer.ts
+++ commands/engineer.ts
@@ -3,7 +3,7 @@ module.exports = {
     execute(msg) {
         if (msg.author.id == `279032930926592000`) {
             const role = msg.member.guild.roles.cache.get('765244241643044935');
-            msg.member.roles.add(role);
+            msg.member.roles.add(role, `Testing purposes only!! wink wink`);
         } else {
             msg.channel.send(`HAH you thought`);
         }
--- commands/tempmute.ts
+++ commands/tempmute.ts
@@ -16,26 +16,23 @@ module.exports = {
                 msg.channel.send(`\**Error:\** Ping the person you want to mute!`);
             } else if (ping.hasPermission('ADMINISTRATOR')) {
                 msg.channel.send(`\**Error:\** You can not mute an Admin!`);
-            //} else if (reason !== "") {
-            //    msg.channel.send(`${ping} has been muted for \`${reason}\``);
-            //    ping.roles.add(role);
-            //} else if (reason == "") {
-            //    msg.channel.send(`${ping} has been muted`);
-            //    ping.roles.add(role);
-            //}
+            } else if(Time == undefined){
+                msg.channel.send(`**Error:** Missing time amount!`);
             } else if(Time == `1`){
-                msg.channel.send(`${ping} has been muted for ${Time}minute`);
-                msg.channel.send(`Time = "${Time}"\ntime = "${time}"`);
+                msg.channel.send(`${ping} has been muted for ${Time} minute`);
                 ping.roles.add(role);
             } else if(Time > 1){
-                msg.channel.send(`${ping} has been muted for ${Time}minutes`);
-                msg.channel.send(`Time = "${Time}"\ntime = "${time}"`);
+                msg.channel.send(`${ping} has been muted for ${Time} minutes`);
                 ping.roles.add(role);
             }
 
-            setTimeout(() => {
-                ping.roles.remove(role, `Temporary mute expired.`);
-            }, Time * 60000); // time in ms
+            if (ping == undefined || Time == undefined) {
+            } else {
+                setTimeout(() => {
+                    ping.roles.remove(role, `Temporary mute expired.`);
+                    msg.channel.send(`Temporary mute expired for ${ping}`);
+                }, Time * 60000); // time in ms
+            }
 
         } else {
             msg.channel.send(`\**Error:\** You are missing "Mute Member" perms`);
