eaedb9b3f2c56c4aa67e0aed7660810369df07cd
Chr1sDev <chris@chr1s.dev>
2020-10-21 11:48:42 -0500

    fix

--- commands/question.ts
+++ commands/question.ts
@@ -4,22 +4,16 @@ module.exports = {
 
         //var url = `https://opentdb.com/api.php?amount=1&category=31&difficulty=easy&type=boolean&token=4dfaa8e46fe332a7cb41f3d700ffead9f7f59512522a24c04d026b24ac41150c`;
         //var url = `https://opentdb.com/api.php?amount=1&category=24&difficulty=hard&type=boolean&token=4dfaa8e46fe332a7cb41f3d700ffead9f7f59512522a24c04d026b24ac41150c`;
-        var url = `https://opentdb.com/api.php?amount=1&category=15&difficulty=easy&type=boolean&encode=url3986&token=4dfaa8e46fe332a7cb41f3d700ffead9f7f59512522a24c04d026b24ac41150c`;
+        var url = `https://opentdb.com/api.php?amount=1&category=15&difficulty=easy&type=boolean&encode=url3986`;
 
         const fetch = require(`node-fetch`);
 
         fetch(url)
         .then(res => res.json())
         .then((api) => {
-
-        if (api.results == null) {
-            msg.channel.send(`**Error:** Something Happened F`);
-        } else {
-
             var str = unescape(`${api.results[0].question}`);
 
             msg.channel.send(`${str}`);
-        }
 
         })
         .catch(err => { throw err });
