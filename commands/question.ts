9d571c4b5839b56bd3a492c0df343028383d7622
Chr1sDev <chris@chr1s.dev>
2020-10-20 22:44:57 -0500

    remove package

--- commands/question.ts
+++ commands/question.ts
@@ -2,10 +2,11 @@ module.exports = {
     name: "question",
     execute(msg) {
 
-        var url = `https://opentdb.com/api.php?amount=1&category=31&difficulty=easy&type=boolean&token=4dfaa8e46fe332a7cb41f3d700ffead9f7f59512522a24c04d026b24ac41150c`;
+        //var url = `https://opentdb.com/api.php?amount=1&category=31&difficulty=easy&type=boolean&token=4dfaa8e46fe332a7cb41f3d700ffead9f7f59512522a24c04d026b24ac41150c`;
+        //var url = `https://opentdb.com/api.php?amount=1&category=24&difficulty=hard&type=boolean&token=4dfaa8e46fe332a7cb41f3d700ffead9f7f59512522a24c04d026b24ac41150c`;
+        var url = `https://opentdb.com/api.php?amount=1&category=15&difficulty=easy&type=boolean&encode=url3986&token=4dfaa8e46fe332a7cb41f3d700ffead9f7f59512522a24c04d026b24ac41150c`;
 
         const fetch = require(`node-fetch`);
-        const entities = require("entities");
 
         fetch(url)
         .then(res => res.json())
@@ -15,9 +16,9 @@ module.exports = {
             msg.channel.send(`**Error:** Something Happened F`);
         } else {
 
-            var str = entities.decodeHTML(`${api.results[0].question}`);
+            var str = unescape(`${api.results[0].question}`);
 
-            msg.channel.send(`"${str}"`);
+            msg.channel.send(`${str}`);
         }
 
         })
