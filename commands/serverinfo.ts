9dd494130caf794e9e240452e408165099cb5da8
Chr1sDev <chris@chr1s.dev>
2020-10-21 17:35:00 -0500

    make it pretty

--- commands/serverinfo.ts
+++ commands/serverinfo.ts
@@ -1,7 +1,28 @@
+const Discord55 = require(`discord.js`);
 module.exports = {
 	name: 'serverinfo',
 	description: '',
 	execute(msg, args) {
-        msg.channel.send(`**Server name:** ${msg.guild.name}\n**Total members:** ${msg.guild.memberCount}`);
+
+		let str = `${msg.guild.createdAt}`;
+		var created = /\([^)]*\)/g;
+		var result = str.match(created).toString();
+		var s = str.replace(result, '')
+
+		const exampleEmbed = new Discord55.MessageEmbed()
+		.setAuthor(`Server Name: ${msg.guild.name}`)
+		.setTitle(`Server Info:`)
+        .setColor('#00FF86')
+		.setFooter(`ID: ${msg.guild.id}`)
+		.addFields(
+			{ name: 'Member Count:', value: `${msg.guild.memberCount}`, inline: true},
+			{ name: `Created At:`, value: `${s}`, inline: true},
+			{ name: `Owner:`, value: `${msg.guild.owner}` },
+			{ name: `Boost Lvl:`, value: `${msg.guild.premiumTier}`, inline: true },
+			{ name: `Number of Boosts:`, value: `${msg.guild.premiumSubscriptionCount}`, inline: true }
+		)
+		.setThumbnail(`${msg.guild.iconURL({ dynamic: true })}`)
+		msg.channel.send(exampleEmbed);
+
 	},
 };
