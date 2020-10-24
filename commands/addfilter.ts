f1c8cdf4549cfe6b05f5dbe693344de6c9a406a1
Chr1sDev <chris@chr1s.dev>
2020-10-24 12:57:03 -0500

    temporarily add to filter

---
+++ commands/addfilter.ts
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "addfilter",
+    execute(msg) {
+
+        var word = require(`../bannedWords.json`);
+
+        var input = msg.content.substr(10).trim();
+
+        word.words.push(input);
+
+        msg.channel.send(`Word added to filter`);
+
+    },
+};
