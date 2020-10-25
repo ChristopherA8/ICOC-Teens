f5041880c70216f261e6a8ebfe4b0a2f84708d8b
Chr1sDev <chris@chr1s.dev>
2020-10-24 22:42:17 -0500

    !game is done pog pog pog

--- commands/game.ts
+++ commands/game.ts
@@ -1,10 +1,9 @@
 const Discord25664 = require(`discord.js`);
 module.exports = {
-    name: "gametest",
+    name: "game",
     execute(msg) {
 
-        //var input = msg.content.substr(5).trim();
-        var input = msg.content.substr(8).trim();
+        var input = msg.content.substr(5).trim();
 
         let url = `https://api.rawg.io/api/games?key=b9e34892707c44919b24ad29cbce52d7&search=${input}`
 
@@ -60,13 +59,27 @@ module.exports = {
 function embed(msg, api, descApi, platforms) {
     const exampleEmbed = new Discord25664.MessageEmbed()
     .setAuthor(`${api.results[0].name}`)
+    .setTitle(``)
     .setColor('#00FF86')
     .setFooter(`Rating: ${api.results[0].rating}/${api.results[0].rating_top}  |  Released: ${api.results[0].released}`)
     .setDescription(``)
     .addFields(
-        { name: `Description:`, value: `"${descApi.description}"`, inline: true },
+        { name: `Description:`, value: `${removeHTML(descApi.description).substring(0,250)}`, inline: true },
         { name: `Platforms:`, value: `${platforms.join(`\n`)}`, inline: true },
     )
     .setThumbnail(`${api.results[0].background_image}`)
     msg.channel.send(exampleEmbed);
+}
+
+
+function removeHTML(str) {
+    return str.replace(`<p>`,``)
+    .replace(`</p>`, ``)
+    .replace(`<h1>`, ``)
+    .replace(`<h2>`, ``)
+    .replace(`<h3>`, ``)
+    .replace(`</h1>`, ``)
+    .replace(`</h2>`, ``)
+    .replace(`</h3>`, ``)
+    .replace(`<br>`, ``);
 }
