e54a39835c4c6399f8ecec1c5ca607c1b6d362bb
Chr1sDev <chris@chr1s.dev>
2020-10-25 23:18:38 -0500

    add command pog

---
+++ commands/rhyme.ts
@@ -0,0 +1,23 @@
+module.exports = {
+    name:"rhyme",
+    execute(msg) {
+        const fetch = require(`node-fetch`);
+        var input = msg.content.substr(6).trim();
+        let url = `https://api.datamuse.com/words?rel_rhy=${input}`;
+
+        fetch(url)
+        .then(res => res.json())
+        .then(api => {
+
+            if (api == "") {
+                msg.channel.send(`**Error:** Missing Word!`)
+            } else {
+                msg.channel.send(`Words that rhyme with *${input}*:\n${api[0].word}`);
+            }
+
+
+        })
+        .catch(console.error);
+
+    },
+};
