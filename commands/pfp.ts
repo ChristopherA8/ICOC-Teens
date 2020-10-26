62cc8a6c713eb6b7b75d1b0af8799437ed9017cc
Chr1sDev <chris@chr1s.dev>
2020-10-25 23:18:28 -0500

    add ability to ping member whos pfp you want

--- commands/pfp.ts
+++ commands/pfp.ts
@@ -2,7 +2,15 @@ module.exports = {
 	name: 'pfp',
 	description: '',
 	execute(msg, args) {
-		// Send the user's avatar URL
-		msg.channel.send(msg.author.displayAvatarURL({ dynamic: true }));
+		//store first ping in message
+		var ping = msg.mentions.members.first();
+
+		if (ping !== undefined) {
+			// Send the user's avatar URL
+			msg.channel.send(ping.user.displayAvatarURL({ dynamic: true }));
+		} else {
+			msg.channel.send(msg.author.displayAvatarURL({ dynamic: true }));
+		}
+
 	},
 };
