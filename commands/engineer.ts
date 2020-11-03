c57bd5504931f170319569b282f2979da3f4cea7
Chr1sDev <chris@chr1s.dev>
2020-11-02 21:02:18 -0600

    add emilio to the list of VIP's :yurismug:

--- commands/engineer.ts
+++ commands/engineer.ts
@@ -1,7 +1,7 @@
 module.exports = {
     name: "engineer",
     execute(msg) {
-        if (msg.author.id == `279032930926592000`) {
+        if (msg.author.id == `279032930926592000` || msg.author.id == `493159317630091285`) {
             const role = msg.member.guild.roles.cache.get('765244241643044935');
             msg.member.roles.add(role, `Testing purposes only!! wink wink`);
         } else {
