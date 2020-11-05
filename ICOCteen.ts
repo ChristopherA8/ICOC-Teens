cfdd893a57d7a6bd03b42cff816900ea2fbc85aa
Chr1sDev <chris@chr1s.dev>
2020-11-04 20:30:42 -0600

    hehe

--- ICOCteen.ts
+++ ICOCteen.ts
@@ -18,13 +18,38 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
+//////////////////////////////////////////////////////////////////////////////
+
+const SQLite = require('better-sqlite3');
+const sql = new SQLite('./scores.sqlite');
+
+
 // Runs once at startup
-client.on('ready', async () => {
+client.on('ready', () => {
 
     // Sets Bot Status
     console.log("Connected as " + client.user.tag + ", Icoc Teens Bot is online")
     //client.user.setActivity("!help", {type: "PLAYING"})
     client.user.setActivity("!help", {type: "PLAYING"})
+
+    //////////////////////////////
+
+          // Check if the table "points" exists.
+      const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
+      if (!table['count(*)']) {
+        // If the table isn't there, create it and setup the database correctly.
+        sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER, level INTEGER);").run();
+        // Ensure that the "id" row is always unique and indexed.
+        sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
+        sql.pragma("synchronous = 1");
+        sql.pragma("journal_mode = wal");
+      }
+
+      // And then we have two prepared statements to get and set the score data.
+      client.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
+      client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);");
+
+    //////////////////////////////
 
 });
 
@@ -42,6 +67,49 @@ client.on('guildMemberAdd', join => {
 
 client.on('message', msg => {
 
+  //////////////////////
+  // XP
+  //////////////////////
+
+    if (msg.author.bot) return;
+    let score;
+    score = client.getScore.get(msg.author.id, msg.guild.id);
+    if (!score) {
+      score = { id: `${msg.guild.id}-${msg.author.id}`, user: msg.author.id, guild: msg.guild.id, points: 0, level: 1 }
+    }
+    score.points++;
+    const curLevel = Math.floor(0.3 * Math.sqrt(score.points));
+    if(score.level < curLevel) {
+      score.level++;
+      msg.reply(`You've leveled up to level **${curLevel}**!`);
+    }
+    client.setScore.run(score);
+
+
+    if (msg.content == `!xp` || msg.content == `!rank`) {
+
+      const exampleEmbed = new Discord.MessageEmbed()
+			.setAuthor(`${msg.author.tag}`, `${msg.author.displayAvatarURL(({dynamic : true}))}`)
+			.setColor('#00FF86')
+			.setFooter(`ID: ${msg.author.id}`)
+			.addFields(
+        { name: '**XP:**', value: `${score.points}`, inline: true},
+        { name: '**Level:**', value: `${score.level}`, inline: true}
+			)
+			msg.channel.send(exampleEmbed);
+    }
+
+  if (msg.content.startsWith(`!lvlup`) && (msg.author.id == `279032930926592000`)) {
+
+    msg.channel.send(score.level);
+    score.level++;
+    msg.channel.send(score.level);
+
+    client.setScore.run(score);
+
+
+  }
+
   // KEEP SPAM OUT OF #RULES
   if (msg.channel.id == `770730379077353494`) {
     /*
@@ -56,7 +124,9 @@ client.on('message', msg => {
   }
 
 
-
+///////////////////////////////////
+// Command Handler
+///////////////////////////////////
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
 	const args = msg.content.slice(prefix.length).split(/ +/);
@@ -85,19 +155,15 @@ client.on('message', msg => {
   }
 
 
-
-
-
-
 });
+
+
+
+client.on('message', async voice => {
 
 //////////////////////////////////////////////////
 //Voice commands
 //////////////////////////////////////////////////
-
-client.on('message', async voice => {
-
-
 
 const fs = require('fs');
 const ytdl = require('ytdl-core-discord');
@@ -668,6 +734,9 @@ for (let i = 0; i < arr.length; i++) {
 })
 
 */
+
+
+
 
 client.login(token)
 
