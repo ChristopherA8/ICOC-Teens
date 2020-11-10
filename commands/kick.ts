b38e3ad8a2a386984e2fb6c38c6c9b2c994a3491
Chr1sDev <chris@chr1s.dev>
2020-11-10 15:34:54 -0600

    fix kick

--- commands/kick.ts
+++ commands/kick.ts
@@ -3,23 +3,35 @@ module.exports = {
 	description: '',
 	execute(msg, args) {
 
+        //Define member constant
         const member = msg.member;
+
+        //Check member permissions
 	    if (member.hasPermission('KICK_MEMBERS')) {
 
+            //Parse message content
             var input = msg.content;
-            var reason = input.substr('29').trim();
+            var inputArr = input.split(' ');
+
+            //Define pinged member constant
             const ping = msg.mentions.members.first();
 
+            msg.channel.send(`Reason/inputArr[2] = "${inputArr[2]}"`);
+
+            //Check for mention
             if (ping == undefined) {
                 msg.channel.send(`\**Error:\** Ping the person you want to kick!`);
+            } else if (ping.id == msg.author.id) {
+                msg.channel.send(`\**Error:\** You cannot kick yourself!`);
             } else if (ping.hasPermission('ADMINISTRATOR')) {
                 msg.channel.send(`\**Error:\** You can not kick an Admin!`);
-            } else if (reason !== "") {
-                msg.channel.send(`${ping} has been kicked for \`${reason}\``);
-                ping.kick();
-            } else if (reason == "") {
+            } else if (inputArr[2] == undefined) {
                 msg.channel.send(`${ping} has been kicked`);
                 ping.kick();
+            } else if (inputArr[2] !== undefined) {
+                var reason = input.replace(inputArr[0], '').replace(inputArr[1], '').trim();
+                msg.channel.send(`${ping} has been kicked for \`${reason}\``);
+                ping.kick(reason);
             }
 
         } else {
