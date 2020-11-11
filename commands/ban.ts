ef6f5ee00bee009b228642b1b8e6795bcf7a9cbe
Chr1sDev <chris@chr1s.dev>
2020-11-10 18:50:06 -0600

    make ban better

--- commands/ban.ts
+++ commands/ban.ts
@@ -3,27 +3,37 @@ module.exports = {
 	description: '',
 	execute(msg, args) {
 
+        //Define member constant
         const member = msg.member;
+
+        //Check member permissions
 	    if (member.hasPermission('BAN_MEMBERS')) {
 
+            //Parse message content
             var input = msg.content;
-            var reason = input.substr('28').trim();
+            var inputArr = input.split(' ');
+
+            //Define pinged member constant
             const ping = msg.mentions.members.first();
 
+            //Check for mention
             if (ping == undefined) {
                 msg.channel.send(`\**Error:\** Ping the person you want to ban!`);
+            } else if (ping.id == msg.author.id) {
+                msg.channel.send(`\**Error:\** You cannot ban yourself!`);
             } else if (ping.hasPermission('ADMINISTRATOR')) {
                 msg.channel.send(`\**Error:\** You can not ban an Admin!`);
-            } else if (reason !== "") {
-                msg.channel.send(`${ping} has been banned for \`${reason}\``);
+            } else if (inputArr[2] == undefined) {
+                msg.channel.send(`${ping} has been baned`);
                 ping.ban();
-            } else if (reason == "") {
-                msg.channel.send(`${ping} has been banned`);
-                ping.ban();
+            } else if (inputArr[2] !== undefined) {
+                var reason = input.replace(inputArr[0], '').replace(inputArr[1], '').trim();
+                msg.channel.send(`${ping} has been baned for \`${reason}\``);
+                ping.ban(reason);
             }
 
         } else {
-            msg.channel.send(`\**Error:\** You are missing "Ban" perms`);
+            msg.channel.send(`\**Error:\** You are missing "ban" perms`);
         }
         
 
