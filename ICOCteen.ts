869c0f607c14eab0e975a50e153d029793681de6
Chr1sDev <chris@chr1s.dev>
2020-11-17 15:22:20 -0600

    cleanup code

--- ICOCteen.ts
+++ ICOCteen.ts
@@ -18,8 +18,6 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
-////////////////////////////////////////////
-
 const SQLite = require('better-sqlite3');
 const sql = new SQLite('./scores.sqlite');
 
@@ -32,27 +30,23 @@ client.on('ready', () => {
     //client.user.setActivity("!help", {type: "PLAYING"})
     client.user.setActivity("to Trash Taste Podcast", {type: "LISTENING"})
 
-    //////////////////////////////
+    // Check if the table "points" exists.
+    const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
+    if (!table['count(*)']) {
+      // If the table isn't there, create it and setup the database correctly.
+      sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER, level INTEGER, name TEXT);").run();
+      // Ensure that the "id" row is always unique and indexed.
+      sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
+      sql.pragma("synchronous = 1");
+      sql.pragma("journal_mode = wal");
+    }
 
-          // Check if the table "points" exists.
-      const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
-      if (!table['count(*)']) {
-        // If the table isn't there, create it and setup the database correctly.
-        sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER, level INTEGER, name TEXT);").run();
-        // Ensure that the "id" row is always unique and indexed.
-        sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
-        sql.pragma("synchronous = 1");
-        sql.pragma("journal_mode = wal");
-      }
-
-      // And then we have two prepared statements to get and set the score data.
-      client.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
-      client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level, name) VALUES (@id, @user, @guild, @points, @level, @name);");
-      //Moderation
-      client.getWarns = sql.prepare("SELECT * FROM moderation WHERE id = ?");
-      client.setWarns = sql.prepare("INSERT OR REPLACE INTO moderation (id, name, warns) VALUES (@id, @name, @warns);");
-
-    //////////////////////////////
+    // And then we have two prepared statements to get and set the score data.
+    client.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
+    client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level, name) VALUES (@id, @user, @guild, @points, @level, @name);");
+    //Moderation
+    client.getWarns = sql.prepare("SELECT * FROM moderation WHERE id = ?");
+    client.setWarns = sql.prepare("INSERT OR REPLACE INTO moderation (id, name, warns) VALUES (@id, @name, @warns);");
 
 });
 
