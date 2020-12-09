78f8f9dd1769f4375da9304accdffa14b4ed46cc
Chr1sDev <chris@chr1s.dev>
2020-12-08 21:40:56 -0600

    remove roles no more hehe

--- commands/removerole.ts
+++ commands/removerole.ts
@@ -3,89 +3,89 @@ module.exports = {
     description: '',
     execute(msg, args) {
 
-        //Declare Constants/Variables
-        const muted = '759587936429277214';
-        var input = msg.content;
-        var roleNameA = input.substr(12);
-        var roleName = roleNameA.trim();
-        var pingRoleNameA = roleName.split(`>`)
-        var pingRoleNameB = pingRoleNameA[1];
+        // //Declare Constants/Variables
+        // const muted = '759587936429277214';
+        // var input = msg.content;
+        // var roleNameA = input.substr(12);
+        // var roleName = roleNameA.trim();
+        // var pingRoleNameA = roleName.split(`>`)
+        // var pingRoleNameB = pingRoleNameA[1];
 
-        let role = msg.guild.roles.cache.find(role => role.name === `${roleName}`);
-        let ping = msg.mentions.members.first();
+        // let role = msg.guild.roles.cache.find(role => role.name === `${roleName}`);
+        // let ping = msg.mentions.members.first();
 
-        if (ping == undefined) {
+        // if (ping == undefined) {
 
-            //Check to see if msg.author is muted
-            if (msg.member.roles.cache.has(muted)) {
+        //     //Check to see if msg.author is muted
+        //     if (msg.member.roles.cache.has(muted)) {
 
-                msg.channel.send(`> \**Error:\** You are muted!`);
+        //         msg.channel.send(`> \**Error:\** You are muted!`);
             
-            } else {
+        //     } else {
 
-                if (role == undefined) {
+        //         if (role == undefined) {
                 
-                    msg.channel.send(`> \**Error:\** Enter a valid role! (Case Sensitive)`);
+        //             msg.channel.send(`> \**Error:\** Enter a valid role! (Case Sensitive)`);
 
-                } else {
+        //         } else {
 
-                    if (msg.member.roles.cache.has(role.id)) {
+        //             if (msg.member.roles.cache.has(role.id)) {
                         
-                        msg.member.roles.remove(role.id);
-                        msg.channel.send(`> "\**${role.name}\**" role has been removed`);
+        //                 msg.member.roles.remove(role.id);
+        //                 msg.channel.send(`> "\**${role.name}\**" role has been removed`);
 
-                    } else {
+        //             } else {
 
-                        msg.channel.send(`> \**Error:\** You don't have this role!`);
+        //                 msg.channel.send(`> \**Error:\** You don't have this role!`);
                     
-                    }
+        //             }
 
-                }
+        //         }
 
-            } 
+        //     } 
 
-        } else {
+        // } else {
 
-            var pingRoleName = pingRoleNameB.trim();
-            let pingRole = msg.guild.roles.cache.find(role => role.name === `${pingRoleName}`);
+        //     var pingRoleName = pingRoleNameB.trim();
+        //     let pingRole = msg.guild.roles.cache.find(role => role.name === `${pingRoleName}`);
 
-            //Check to see if "ping" is muted
-            if (ping.roles.cache.has(muted)) {
+        //     //Check to see if "ping" is muted
+        //     if (ping.roles.cache.has(muted)) {
 
-                msg.channel.send(`> \**Error:\** ${ping} is muted!`);
+        //         msg.channel.send(`> \**Error:\** ${ping} is muted!`);
             
-            } else {
+        //     } else {
 
-                if (pingRole == undefined) {
+        //         if (pingRole == undefined) {
                 
-                    msg.channel.send(`> \**Error:\** Enter a valid role! (Case Sensitive)`);
+        //             msg.channel.send(`> \**Error:\** Enter a valid role! (Case Sensitive)`);
 
-                } else {
+        //         } else {
 
-                    if (ping.roles.cache.has(pingRole.id)) {
+        //             if (ping.roles.cache.has(pingRole.id)) {
 
-                        if (msg.member.hasPermission("MANAGE_ROLES")) {
+        //                 if (msg.member.hasPermission("MANAGE_ROLES")) {
                             
-                            ping.roles.remove(pingRole.id);
-                            msg.channel.send(`> "\**${pingRole.name}\**" role has been removed`);
+        //                     ping.roles.remove(pingRole.id);
+        //                     msg.channel.send(`> "\**${pingRole.name}\**" role has been removed`);
 
-                        } else {
+        //                 } else {
 
-                            msg.channel.send(`> \**Error:\** ${msg.author} does not have permission "Manage Roles"!`);
+        //                     msg.channel.send(`> \**Error:\** ${msg.author} does not have permission "Manage Roles"!`);
 
-                        }
+        //                 }
                         
-                    } else {
+        //             } else {
 
-                        msg.channel.send(`> \**Error:\** ${ping} doesn't have this role!`);
+        //                 msg.channel.send(`> \**Error:\** ${ping} doesn't have this role!`);
 
-                    }
+        //             }
 
-                }
+        //         }
 
-            }
+        //     }
 
-        }
+        // }
 
     },
 
