4b18fd496b9787f11472799b1b2d20d5925939b8
Chr1sDev <chris@chr1s.dev>
2020-12-27 22:24:02 -0600

    pogu

--- ICOCteen.ts
+++ ICOCteen.ts
@@ -1011,5 +1011,39 @@ function warnEmbed(channel, member, reason, warner) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   /* =-=-=-=-=-=-=-= Leaderboard Server =-=-=-=-=-=-=-= */
+//   const express = require('express');
+//   const app = express();
+
+//   app.get("/", function (req, res) {
+//       res.sendFile(__dirname + "/index.html");
+//   });
+//   app.listen(420, function () {
+//       console.log("Server is running on localhost:420");
+//   });
+// /* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-= */
+
 client.login(token)
 
