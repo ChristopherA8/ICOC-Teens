6a92da382b2d694dc753c4c59875e55879ed5181
Chr1sDev <chris@chr1s.dev>
2021-01-11 15:40:11 -0600

    :intjallah:

--- modules/messageListener.ts
+++ modules/messageListener.ts
@@ -13,5 +13,12 @@ module.exports = {
           msg.channel.send(`airhorn airhorn airhorn`);
         }
 
+        // /* =-=-=-= Keep Messages out of #the-dungeon =-=-=-= */
+        // if (msg.channel.id == `768931736414584902`) {
+        //     if ((msg.author.id !== `620438897217896459`) && (msg.author.id !== `279032930926592000`)) {
+        //         msg.delete();
+        //     }
+        // }
+
     },
 };
