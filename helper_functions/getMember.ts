a04d1e40e602bd2657ef06cf49059860b4faa808
chr1sdev <christopheranderson3@me.com>
2021-04-16 00:56:19 +0000

    fix something... I think

--- helper_functions/getMember.ts
+++ helper_functions/getMember.ts
@@ -1,5 +1,5 @@
 const getMember = (id) => {
-    const { client } = require('../ICOCteen.ts');
+    const { client } = require('../ICOCteen.js');
     const guild = client.guilds.cache.get('698590629344575500')
     return guild.members.cache.get(id);
 }
